9a70e6d2a75d5876a3229b2198274069fe712199
sunil kumar sharma <sunil.krsharma.cd.mec22@itbhu.ac.in>
2024-10-13 09:08:22 +0530

    first commit

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -8,50 +8,14 @@ import eth from '../eth.png'
 import './Exchanges.css'
 import OurModel from './OurModel'
 const Exchanges = () => {
-  const [loading, setLoading]=useState(true)
-  const[exchanges, setExchanges]=useState([])
-  useEffect(()=>{
-    const getExchangesData=async()=>{
-      const {data} =await axios.get(`${Baseurl}/exchanges`)
-      console.log(data)
-      setExchanges(data)
-      setLoading(false)
-    }
-    getExchangesData() 
-  },[])
-  return (
-    
-   <>
-     {
-      loading ? <Loader/> : <> 
-       <Header/>
-       <OurModel/> 
-   <div>
-     {
-      exchanges.map((item,i)=>{
-        return(
-          <div key={i} className='ex-cards'>
-          <div className="image">
-            <img height={"80px"} src={item.image} alt="" />
-          </div>
-          <div className="name">
-              {item.name}
-          </div>
-          <div className="price">
-               {item.trade_volume_24h_btc.toFixed(0)}
-          </div>
-          <div className="rank">
-               {item.trust_score_rank}
-          </div>
-       </div>
-        )
-      })
-     }
-   </div>
-      </>
-     }
-   </>
-  )
+   return (
+         <>  
+
+         sunil 
+         
+         </>
+   )
+  
 }
 
 export default Exchanges
